49100e97403fd87d7f9fdb7dbeec34f8c934adc2
fbarda <arda.aydin@operationsilkscarf.com>
2020-06-07 16:57:56 +0300

    Small config update
    + Added an extra config file (in json).
    + Updated JSDOC to include our extra config.
    + indentation changes

--- config_handler.js
+++ config_handler.js
@@ -1,17 +1,19 @@
 let cachedConfig;
 
 /**
- * @returns {{token:string,donators:Array<string>}}
+ * @returns {{token:string,donators:Array<string>,maxDiceSide:number)}}
  * @param {Array<string>} args 
  */
 
-function config_getter  (args) {
-    if(cachedConfig){return cachedConfig;}
-    if (args.some(string => string === "local-mode")) {
-         cachedConfig = require("./local_config_handler");
-    } else {
-         cachedConfig = require("./heroku_config_handler");
-    }
-    return cachedConfig;
+function config_getter(args) {
+     if (cachedConfig) { return cachedConfig; }
+     if (args.some(string => string === "local-mode")) {
+          cachedConfig = require("./local_config_handler");
+     } else {
+          cachedConfig = require("./heroku_config_handler");
+     }
+     const extraConfig = require("./extra_config.json");
+     Object.assign(cachedConfig, extraConfig);
+     return cachedConfig;
 }
-exports= module.exports = config_getter;
+exports = module.exports = config_getter;
