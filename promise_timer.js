87a0bf3015a09f593b87ecfee0581b0b29d69d15
fbarda <arda.aydin@operationsilkscarf.com>
2020-06-07 17:01:16 +0300

    Exported promiseTimer-er

---
+++ promise_timer.js
@@ -0,0 +1,14 @@
+/**
+ * @desc Calls a function every `delay` milliseconds until the promise resolves. The callback will be called at least once.
+ * @param {Promise<*>} promise - A promise that will eventually resolve.
+ * @param {function()=>void} callback A callback function that will have no arguments.
+ * @param {number} delay The delay of calling the callback, in milliseconds.
+ */
+async function promiseTimerContainer(promise, callback, delay) {
+    setImmediate(callback);
+    const timerID = setInterval(callback, delay);
+    await promise;
+    clearInterval(timerID);
+    return promise;
+}
+exports = module.exports = promiseTimerContainer;
